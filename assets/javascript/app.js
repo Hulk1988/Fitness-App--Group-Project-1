40bb29ec52b69a40eeb531c92dfa38f122b25aa2
andrew <andrewjthomsen@yahoo.com>
2019-01-17 19:26:04 -0800

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// weather api
+function getWeather() {
+    let cityVar;
+    var ajaxUrl = "https://openweathermap.org/find?q=" + topic + "&api_key=f4012622bd78ad3173b7526c476fb82a"  // test by throwing url into chrome
+    $.ajax({ //objects made up of methods, properties, and key value pairs
+        url: ajaxUrl,
+        method: "GET",
+    //api key: f4012622bd78ad3173b7526c476fb82a
+    //firebase setup
+
+}
